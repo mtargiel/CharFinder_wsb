5609b431ed640a094c25758a01b66f7c9e980b6d
Michal Targiel <mtargiel@gmail.com>
2017-05-12 00:16:47 +0200

    v2

---
+++ CharFinder/CharFinder/Find.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharFinder
+{
+    class Find
+    {
+        private string _zdanie;
+
+        public string Zdanie
+        {
+            get { return _zdanie; }
+            set { _zdanie = value; }
+        }
+
+        private char _znak;
+
+        public char Znak
+        {
+            get { return _znak; }
+            set { _znak = value; }
+        }
+
+        public Find(string zdanie, char znak)
+        {
+            Zdanie = zdanie;
+            Znak = znak;
+        }
+
+        public int policzZnaki()
+        {
+
+            return _zdanie.Count(x => x == _znak); ;
+        }
+    }
+}
---
+++ CharFinder/CharFinder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz dowolne zdanie");
+            string zdanie = Console.ReadLine();
+            Console.WriteLine("Jakiego znaku szukać?");
+            char znak;
+            char.TryParse(Console.ReadLine(), out znak);
+            Find find = new Find(zdanie, znak);
+            Console.WriteLine(find.policzZnaki());
+            Console.ReadKey();
+        }
+    }
+}
